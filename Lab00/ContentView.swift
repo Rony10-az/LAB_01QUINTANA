ecc9cfc70cce8e79d035848dd2cdbd2119b757a3
Rony Quintana <ronyjhost@gmail.com>
2026-08-20 12:49:44 -0500

    Feat: Agregar etiqueta version manual

--- Lab00/ContentView.swift
+++ Lab00/ContentView.swift
@@ -9,18 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-    VStack(spacing: 20) {
-    Image(systemName: "swift")
-    .imageScale(.large)
-    .foregroundStyle(.orange)
-    .font(.system(size: 60))
-    Text("¡Hola, iOS Avanzado!")
-    .font(.largeTitle)
-    .fontWeight(.bold)
-    Text("Mi primer commit desde Xcode")
-    .font(.subheadline)
-    .foregroundColor(.gray)
-    }
+        VStack(spacing: 20) {
+        Image(systemName: "swift")
+        .imageScale(.large)
+        .foregroundStyle(.orange)
+        .font(.system(size: 60))
+        Text("¡Hola, iOS Avanzado!")
+        .font(.largeTitle)
+        .fontWeight(.bold)
+        Text("Mi primer commit desde Xcode")
+        .font(.subheadline)
+        .foregroundColor(.gray)
+        Text("Versión MANUAL - Hecha sin IA")
+        .font(.headline)
+        .foregroundColor(.blue)
+        }
     .padding()
     }
 }
